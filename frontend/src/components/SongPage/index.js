97c22a461b27cdbf1553b04b43119d89c0534922
Karl Felter <kbf12897@gmail.com>
2022-02-09 18:39:39 -0500

    songpage css

--- frontend/src/components/SongPage/index.js
+++ frontend/src/components/SongPage/index.js
@@ -12,6 +12,21 @@ function SongPage() {
         <div>
             <div className="song-info">
                 <h3 className="song-title">{song.title}</h3>
+                <img
+                    className="no-song-img"
+                    src="https://cdn2.iconfinder.com/data/icons/audio-files-essential/48/v-30-512.png"
+                />
+                <div className="comment-div">
+                    <h4 className="comment-label">Comments</h4>
+                    <div className="comment-input-button">
+                        <input
+                            className="comment-input"
+                            placeholder="Write a comment"
+                            required
+                        ></input>
+                        <button className="comment-button">Submit</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
