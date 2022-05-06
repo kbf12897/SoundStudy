5e88244abebf576b88f14d9a91d7d87b7ecb788c
Karl Felter <kbf12897@gmail.com>
2022-05-06 11:39:19 -0400

    seeds

--- backend/db/seeders/20220208164146-userSeeds.js
+++ backend/db/seeders/20220208164146-userSeeds.js
@@ -10,16 +10,19 @@ module.exports = {
                     username: "lofiGirl",
                     email: "lofiGirl@lofi.com",
                     hashedPassword: bcrypt.hashSync("password"),
+                    profileImg: 'https://www.tubefilter.com/wp-content/uploads/2022/03/lofi-girl-college-scholarships-1400x825.jpg',
                 },
                 {
                     username: "lofiGuy",
                     email: "lofiGuy@lofi.com",
                     hashedPassword: bcrypt.hashSync("password"),
+                    profileImg: 'https://i.ytimg.com/vi/8RPCP77xPak/maxresdefault.jpg'
                 },
                 {
                     username: "lofiMaker",
                     email: "lofiMaker@lofi.com",
                     hashedPassword: bcrypt.hashSync("password"),
+                    profileImg: 'https://snworksceo.imgix.net/tsn/fde0889d-4c5c-4e32-b12d-4edd009adda9.sized-1000x1000.png?w=1000'
                 },
             ],
             {}
