388545cf2e8e36b2426bdbb63fbc9d2453528c49
Karl Felter <kbf12897@gmail.com>
2022-05-19 11:42:46 -0400

    songs on user page;

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -1,7 +1,9 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { getSongs } from '../../store/songs';
+import AudioPlayer from '../AudioPlayer';
 import './UserPage.css';
+import { NavLink } from 'react-router-dom';
 
 
 const UserPage = () => {
@@ -24,7 +26,21 @@ const UserPage = () => {
                     PAGE NAVIGATION
                 </div>
                 <div className='user-uploaded-songs'>
-                    USER UPLOADED SONGS
+                    {songs.map((song) => {
+                        return (
+                            <div className={song.id}>
+                                <NavLink to={`/songs/${song.id}`}>
+                                    <img
+                                        className='user-page-song-img'
+                                        src={song.songImg}
+                                        alt='song-img'
+                                    />
+                                </NavLink>
+                                <div>{song.title}</div>
+                                <AudioPlayer song={song}/>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </div>
